d587a21c108e7d2cd3cfecbe31d34dacfadc11de
Tomas Hogenauer <8917681+tomashoge@users.noreply.github.com>
2025-04-07 07:19:34 +0200

    implement service

---
+++ Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using SQLiteDemo.Models;
+
+namespace SQLite_demo.Services
+{
+    class BaseService
+    {
+        protected static SQLiteAsyncConnection dbConnection;
+        private static string dbPath = Path.Combine(Environment.CurrentDirectory, "Person.db");
+
+        protected async Task SetUpDatabase()
+        {
+            if (dbConnection == null)
+            {
+                dbConnection = new SQLiteAsyncConnection(dbPath);
+                await dbConnection.CreateTableAsync<PersonModel>();
+            }
+        }
+    }
+}
---
+++ Services/PersonService.cs
@@ -0,0 +1,31 @@
+﻿using SQLiteDemo.Models;
+
+namespace SQLite_demo.Services
+{
+    class PersonService : BaseService
+    {
+        public async Task<int> AddAsync(PersonModel PersonModel)
+        {
+            await SetUpDatabase();
+            return await dbConnection.InsertAsync(PersonModel);
+        }
+
+        public async Task<int> DeleteAsync(PersonModel PersonModel)
+        {
+            await SetUpDatabase();
+            return await dbConnection.DeleteAsync(PersonModel);
+        }
+
+        public async Task<List<PersonModel>> GetAsync()
+        {
+            await SetUpDatabase();
+            return await dbConnection.Table<PersonModel>().ToListAsync();
+        }
+
+        public async Task<int> UpdateAsync(PersonModel PersonModel)
+        {
+            await SetUpDatabase();
+            return await dbConnection.UpdateAsync(PersonModel);
+        }
+    }
+}
