5dd5cf155f918a2372e024f0fa191d3e24aed202
Tomas Hogenauer <8917681+tomashoge@users.noreply.github.com>
2025-04-07 07:20:10 +0200

    use example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SQLite_demo.Services;
+using SQLiteDemo.Models;
+
+var dbService = new PersonService();
+
+Console.WriteLine("---Init db content---");
+var dbContent = await dbService.GetAsync();
+dbContent.ForEach(x => Console.WriteLine(x.FullName));
+
+Console.WriteLine("---Add record---");
+await dbService.AddAsync(new PersonModel() { FirstName = "David", LastName = "Smith" });
+dbContent = await dbService.GetAsync();
+dbContent.ForEach(x => Console.WriteLine(x.FullName));
+
+Console.WriteLine("---Delete first record---");
+await dbService.DeleteAsync(dbContent.First());
+dbContent = await dbService.GetAsync();
+dbContent.ForEach(x => Console.WriteLine(x.FullName));
+
+Console.WriteLine("---Rename first record---");
+var firstPerson = dbContent.First();
+firstPerson.FirstName = "Tomas";
+await dbService.UpdateAsync(firstPerson);
+dbContent = await dbService.GetAsync();
+dbContent.ForEach(x => Console.WriteLine(x.FullName));
+
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SQLite-demo
+Basic example of init and use SQLite database
