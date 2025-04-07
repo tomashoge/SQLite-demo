a947f2155fe16199c99b2c462460e0e43b64c757
Tomas Hogenauer <8917681+tomashoge@users.noreply.github.com>
2025-04-07 07:19:05 +0200

    add model

---
+++ Models/PersonModel.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteDemo.Models
+{
+    public class PersonModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{PersonId} {FirstName} {LastName}";
+    }
+}
